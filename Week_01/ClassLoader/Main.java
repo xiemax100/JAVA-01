82305bf6a09b68f2719a60c5559cd6fb3f3b5766
xiemax100 <xiemax100@yeah.net>
2021-01-22 21:59:39 +0800

    improve class loader program

--- Week_01/ClassLoader/Main.java
+++ Week_01/ClassLoader/Main.java
@@ -9,7 +9,14 @@ public class Main {
             this.classPath = classPath;
         }
 
-        private byte[] loadByte(String name) throws Exception {
+        private byte[] decode(byte[] bytes) {
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+            return bytes;
+        }
+
+        private byte[] loadBytes(String name) throws Exception {
             FileInputStream fis = new FileInputStream(classPath + "/" + name + ".xlass");
             int len = fis.available();
             byte[] data = new byte[len];
@@ -21,8 +28,8 @@ public class Main {
 
         protected Class<?> findClass(String name) throws ClassNotFoundException {
             try {
-                byte[] data = loadByte(name);
-                return defineClass(name, data, 0, data.length);
+                byte[] data = loadBytes(name);
+                return defineClass(name, decode(data), 0, data.length);
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new ClassNotFoundException();
